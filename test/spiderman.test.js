18f4f18a0dd468f29421f60af5512f2fa100f1e6
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-04 19:04:04 -0500

    Add method getInfo

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -13,6 +13,6 @@ describe("Unit Test for Spiderman class", () => {
     });
     test('2 Use the method getInfo()', () => {
         const tomHolland = new Spiderman('Spiderman Marvel', 25, 'Tom Holland', 5, 'Marvel')
-        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studuio.")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio.")
     });
 })
